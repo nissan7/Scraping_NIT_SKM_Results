5059daa2671674d36a9a631894837374cfa35615
Sandeep <sdpnis7@gmail.com>
2018-07-18 21:57:21 +0530

    scrping unified to cse dept only

--- README.md
+++
@@ -1,2 +0,0 @@
-# Scraping_NIT_SKM_Results
-scraping results of NIT Sikkim from official website.Repo contains Python script. 
--- script.py
+++ script.py
@@ -15,41 +15,34 @@ def roll2sem(roll):
 		
 
 if __name__=='__main__':
-	#semester=input("choose the semester\n0-All\n1-FIRST\n2-SECOND\n3-THIRD\n4-FOURTH\n5-FIFTH\n6-SIXTH\n7-SEVENTH\nEIGHTH\n")
-	#department=input("choose the dept\n0-All\n1-cse\n2-eee\n3-ece\n4-me\n5ce\n6-bio-tech")
-	
 	#print semester,department
-	depts=['cse','eee','ece','me','ce','bio-tech']
 	sems=['FIRST','SECOND','THIRD','FOURTH','FIFTH','SIXTH','SEVENTH','EIGHTH']
 	
 	br=mechanize.Browser()
 	response=br.open(tarurl)
 	
-	if 1:
-		for dept in depts:
-			fil=open(dept+".txt","r").read()
-			rolls=fil.split("\r\n")
-		
-			for roll in rolls:
-				if roll !='':
-					semcontrol=roll2sem(roll)
-				else:
-					semcontrol=0
-				for i in range(semcontrol):
-					temp=br.click_link(text='Results')
-					br.open(temp)
-					br.select_form(nr=0)
-					print "fetching result of "+roll
-					br.form['roll']=roll
-					control=br.form.find_control("year")
-					for item in control.items:
-						if item.name==sems[i]:
-							item.selected=True
-					status=br.submit()
-					result_html=status.read()
-					
-					fnw.fetch_and_write(result_html,dept)
-					br.back()
+	fil=open("cse.txt","r").read()
+	rolls=fil.split("\r\n")
+	dept="cse"
+	for roll in rolls:
+		print "fetching result of "+roll
+		if roll !='':
+			semcontrol=roll2sem(roll)
+		else:
+			semcontrol=0
+		for i in range(semcontrol):
+			temp=br.click_link(text='Results')
+			br.open(temp)
+			br.select_form(nr=0)
+			br.form['roll']=roll
+			control=br.form.find_control("year")
+			for item in control.items:
+				if item.name==sems[i]:
+					item.selected=True
+			status=br.submit()
+			result_html=status.read()			
+			fnw.fetch_and_write(result_html,dept)
+			br.back()
 		
 
 
